2efa4be0ddfc2f0bcfd0870fb2d41b7142bdf2ca
Joao S O Bueno <gwidion@gmail.com>
2019-01-26 18:26:59 -0200

    Maps reading, pushing objects, objects vanishing

--- homekeeper/__main__.py
+++ homekeeper/__main__.py
@@ -11,7 +11,6 @@ BG_COLOR = 0, 0, 0
 BLOCK_SIZE = WIDTH / 32
 
 SCREEN = None
-TILES = None
 
 class Interrupt(BaseException):
     pass
@@ -31,46 +30,166 @@ def handle_input():
 
 
 class Base(pygame.sprite.Sprite):
+    tile_registry = {}
     color = BG_COLOR
-    def __init__(self, pos=(0,0)):
+
+    def __init__(self, board, pos=(0,0)):
+        self.board = board
+        self.x, self.y = pos
         self.step = BLOCK_SIZE
         self.rect = pygame.Rect((pos[0] * BLOCK_SIZE, pos[1] * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE))
         super().__init__()
         self.image = pygame.surface.Surface((BLOCK_SIZE, BLOCK_SIZE))
         self.image.fill(self.color)
+        self.board[self.x, self.y] = self
+        self.previous = None
+
+    def __hash__(self):
+        return hash((self.x, self.y))
 
 
-class Empty(Base):
+
+    def movable(self, direction, pushing=False, count=0):
+        new_x = self.x + direction[0]
+        new_y = self.y + direction[1]
+        if not 0 <= new_x < self.board.width or not 0 < new_y < self.board.height:
+            return False
+
+        new_pos = self.board[new_x, new_y]
+        if not new_pos.traversable:
+            return False
+
+        if not new_pos.pushable:
+            return True
+
+        if not pushing:
+            return True
+
+        if count > 0 and new_pos.movable(direction, pushing, count - 1):
+            return True
+
+        return False
+
+    def move(self, direction, pushing, count):
+        if not self.movable(direction, pushing, count):
+            return
+        new_x = self.x + direction[0]
+        new_y = self.y + direction[1]
+        new_pos = self.board[new_x, new_y]
+        if new_pos.pushable and pushing:
+            new_pos.move(direction, pushing, count - 1)
+
+        self.board[self.x, self.y] = self.previous if self.previous else Empty(self.board, (self.x, self.y))
+        self.x = new_x
+        self.y = new_y
+        self.previous = self.board[self.x, self.y]
+        self.board[self.x, self.y] = self
+        self.moved(direction)
+
+    def kill(self):
+        super().kill()
+        del self.board[self.x, self.y]
+
+    def moved(self, direction):
+        pass
+
+
+    def __init_subclass__(cls):
+        if "tile_char" in cls.__dict__:
+            cls.tile_registry[cls.tile_char] = cls
+
+class GameObject(Base):
+    traversable = True
+    pushable = False
+
+
+class Empty(GameObject):
     color = 128, 128, 128
-    pass
+    tile_char = " "
+
+
+class Wall(GameObject):
+    color = 255, 255, 0
+    traversable = False
+    tile_char = "*"
+
+
+class Dirty(GameObject):
+    color = 0, 0, 255
+    traversable = True
+    pushable = True
+    tile_char = "A"
+
+    def moved(self, direction):
+        if not (direction[0] or direction[1]):
+            return
+
+        equal_neighbours = self.get_equal_neighbours()
+        if len(equal_neighbours) < 4:
+            return
+        score_bonus = 1
+        score = 0
+        for neighbour in equal_neighbours:
+            score += score_bonus
+            score_bonus *= 2
+            neighbour.kill()
+
+        self.board.score += score
+        print(self.board.score)
+
+    def get_equal_neighbours(self, group = None, seen=None):
+        if seen == None:
+            seen = {(self.x, self.y),}
+            group = {self,}
+        for direction in ((-1, 0), (1, 0), (0, -1), (-0, 1)):
+            new_pos = self.x + direction[0], self.y + direction[1]
+            if new_pos in seen:
+                continue
+            obj = self.board.__getitem__(new_pos)
+            if type(obj) == type(self):
+                group.add(obj)
+                seen.add(new_pos)
+                obj.get_equal_neighbours(group, seen)
+        return group
+
+
+
+
 
 
 class Board:
-    def __init__(self, width=32, height=24):
-        global TILES
-        TILES  = pygame.sprite.Group()
+    def __init__(self, width=32, height=24, map=""):
         self.data = [None] * width * height
         self.width = width
         self.height = height
         self.clear()
+        if map:
+            self.load_map(map)
+        self.score = 0
 
     def clear(self):
         for x, y, _ in self:
-            empty = Empty((x, y))
-            TILES.add(empty)
+            empty = Empty(self, (x, y))
+
+
+    def load_map(self, mapfile):
+        with mapfile.open() as map_:
+            for y, row in enumerate(map_):
+                for x, char in enumerate(row):
+                    if x >= self.width:
+                        continue
+                    cls = Base.tile_registry.get(char, Empty)
+                    self[x, y] = cls(self, (x, y))
 
 
     def __getitem__(self, item):
         return self.data[item[1] * self.width + item[0]]
 
     def __setitem__(self, item, value):
-        if isinstance(self[item], Base):
-            self[item].kill()
         self.data[item[1] * self.width + item[0]] = value
 
     def __delitem__(self, item):
-        self[item].kill()
-        self[item] = Empty(item)
+        self[item] = Empty(self, item)
 
     def __iter__(self):
         for x in range(self.width):
@@ -80,17 +199,33 @@ class Board:
     def draw(self, screen):
         pass
 
+    def update(self, screen):
+        for x, y, block in self:
+            screen.blit(block.image, (block.x * BLOCK_SIZE, block.y * BLOCK_SIZE))
 
 
-class Character(Base):
+class Character(GameObject):
     color = 255, 0, 0
+    traversable = False
+    pushable = False
+    move_delay = 3
+
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw)
+        self.move_count = self.move_delay
 
 
     def update(self, keys):
-        self.rect.x += self.step * (keys[pg.K_RIGHT] - keys[pg.K_LEFT])
-        self.rect.y += self.step * (keys[pg.K_DOWN] - keys[pg.K_UP])
-        self.rect.x %= WIDTH
-        self.rect.y %= HEIGHT
+        self.move_count -= 1
+
+        if self.move_count > 0:
+            return
+        direction = (keys[pg.K_RIGHT] - keys[pg.K_LEFT]), (keys[pg.K_DOWN] - keys[pg.K_UP])
+
+        if self.movable(direction, keys[pg.K_SPACE], 1):
+            if direction[0] or direction[1]:
+                self.move_count = self.move_delay * (1 + keys[pg.K_SPACE])
+            self.move(direction, keys[pg.K_SPACE], 1)
 
 
 
@@ -99,21 +234,16 @@ def frame_clear():
 
 
 def scene_main():
-    global TILES
     clk = pg.time.Clock()
-    character = pygame.sprite.Group()
-    character.add(Character())
-    board = Board()
-    tiles = TILES
+    board = Board(map=Path("maps/bedroom.txt"))
+    character = Character(board, (1, 1))
 
 
     while True:
         frame_clear()
         keys = handle_input()
-        tiles.update()
         character.update(keys)
-        tiles.draw(SCREEN)
-        character.draw(SCREEN)
+        board.update(SCREEN)
         pg.display.flip()
         clk.tick(30)
 
