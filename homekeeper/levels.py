2c7b5a527d9cc982fe25b0394f0e531d051a8c6d
Joao S O Bueno <gwidion@gmail.com>
2019-01-28 01:10:15 -0200

    Update README

--- homekeeper/levels.py
+++ homekeeper/levels.py
@@ -2,7 +2,7 @@ levels = [{
         "map": "bedroom_small.txt",
         "number": 0,
         "classes": [("Dirty", 1), ("Dirty2", 1)],
-        "total_time": 120,
+        "total_time": 100,
         "dirty_pop_rate": 3, # seconds per block
         "dirty_pop_noise": 0,
         "goal": 20, # total blocks to vanish
@@ -12,7 +12,7 @@ levels = [{
         "map": "bedroom_small.txt",
         "number": 1,
         "classes": [("Dirty", 1), ("Dirty2", 1), ("Dirty3", 1), ("Dirty4", 1)],
-        "total_time": 150,
+        "total_time": 100,
         "dirty_pop_rate": 2, # seconds per block
         "dirty_pop_noise": 0,
         "goal": 40, # total blocks to vanish
@@ -22,7 +22,7 @@ levels = [{
         "map": "bedroom_small_with_bed.txt",
         "number": 2,
         "classes": [("Dirty", 1), ("Dirty2", 1), ("Dirty3", 1), ("Dirty4", 1)],
-        "total_time": 200,
+        "total_time": 140,
         "dirty_pop_rate": 1.5, # seconds per block
         "dirty_pop_noise": 0.2,
         "goal": 40, # total blocks to vanish
@@ -32,7 +32,7 @@ levels = [{
         "map": "bedroom_small_with_bed.txt",
         "number": 3,
         "classes": [("Dirty", 1), ("Dirty2", 1), ("Dirty3", 1), ("Dirty4", 1), ("Dirty5", 1)],
-        "total_time": 200,
+        "total_time": 100,
         "dirty_pop_rate": 1, # seconds per block
         "dirty_pop_noise": .5,
         "goal": 60, # total blocks to vanish
@@ -42,7 +42,7 @@ levels = [{
         "map": "bedroom_small_with_bed.txt",
         "number": 4,
         "classes": [("Dirty", 1), ("Dirty2", 1), ("Dirty3", 1), ("Dirty4", 1), ("Dirty5", 1)],
-        "total_time": 200,
+        "total_time": 100,
         "dirty_pop_rate": 0.8, # seconds per block
         "dirty_pop_noise": .3,
         "goal": 80, # total blocks to vanish
